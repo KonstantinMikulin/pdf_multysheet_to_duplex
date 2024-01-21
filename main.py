b35886a07514b54e89653565898cc4e7a8462059
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2024-01-21 18:24:53 +0300

    Done

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
 import PyPDF2
 import os
 
-input_pdf = 'C:/Users/user/Desktop/pdf_multysheet/test_pdf.pdf'
-store_folder = 'C:/Users/user/Desktop/pdf_multysheet/pdf_duplex'
+from config import SOURCE_DIR, RESULT_DIR
 
 
-def split_pdf(input_file, output_folder):
-    with open(input_file, 'rb') as file:
-        pdf_reader = PyPDF2.PdfReader(file)
+def split_pdf(input_folder, output_folder):
+    if not os.path.exists(RESULT_DIR):
+        os.makedirs(RESULT_DIR)
 
-        if len(pdf_reader.pages) % 2 != 0:
-            print("Error: The number of pages in the PDF must be even for duplex scanning.")
-            return
+    for file_name in os.listdir(SOURCE_DIR):
+        # Check if the file is an .pdf
+        if file_name.endswith('.pdf'):
+            # Construct the full path to the PDF file
+            input_file = os.path.join(SOURCE_DIR, file_name)
 
-        for i in range(0, len(pdf_reader.pages), 2):
-            pdf_writer = PyPDF2.PdfWriter()
-            pdf_writer.add_page(pdf_reader.pages[i])
-            pdf_writer.add_page(pdf_reader.pages[i + 1])
+            with open(input_file, 'rb') as file:
+                pdf_reader = PyPDF2.PdfReader(file)
 
-            output_file = os.path.join(output_folder, f"result_{i//2 + 1}.pdf")
+                if len(pdf_reader.pages) % 2 != 0:
+                    print("Error: The number of pages in the PDF must be even for duplex scanning.")
+                    return
 
-            with open(output_file, 'wb') as output:
-                pdf_writer.write(output)
+                for i in range(0, len(pdf_reader.pages), 2):
+                    pdf_writer = PyPDF2.PdfWriter()
+                    pdf_writer.add_page(pdf_reader.pages[i])
+                    pdf_writer.add_page(pdf_reader.pages[i + 1])
+
+                    output_file = os.path.join(output_folder, f"result_{i//2 + 1}.pdf")
+
+                    with open(output_file, 'wb') as output:
+                        pdf_writer.write(output)
 
 
 if __name__ == "__main__":
-    if not os.path.exists(store_folder):
-        os.makedirs(store_folder)
-
-    split_pdf(input_pdf, store_folder)
+    split_pdf(SOURCE_DIR, RESULT_DIR)
