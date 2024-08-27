73f9a54eadc05ec11b753293695e80c63970cfb1
konstantin_mikulin <mikulin.konstantin@gmail.com>
2024-08-27 11:51:22 +0300

    change library PyPDF2 to pypdf

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import PyPDF2
 import os
 from datetime import datetime
+import pypdf
 
 from config import INPUT_DIR, RESULT_DIR
 
@@ -9,6 +9,9 @@ def split_pdf(input_dir, output_dir):
     if not os.path.exists(RESULT_DIR):
         os.makedirs(RESULT_DIR)
 
+    print(input_dir)
+    print(output_dir)
+    
     for file_name in os.listdir(input_dir):
         # Check if the file is an .pdf
         if file_name.endswith('.pdf'):
@@ -16,14 +19,14 @@ def split_pdf(input_dir, output_dir):
             input_file = os.path.join(input_dir, file_name)
 
             with open(input_file, 'rb') as file:
-                pdf_reader = PyPDF2.PdfReader(file)
+                pdf_reader = pypdf.PdfReader(file)
 
                 if len(pdf_reader.pages) % 2 != 0:
                     print("Error: The number of pages in the PDF must be even for duplex scanning.")
                     return
 
                 for i in range(0, len(pdf_reader.pages), 2):
-                    pdf_writer = PyPDF2.PdfWriter()
+                    pdf_writer = pypdf.PdfWriter()
                     pdf_writer.add_page(pdf_reader.pages[i])
                     pdf_writer.add_page(pdf_reader.pages[i + 1])
                     time_stamp = str(datetime.now()).replace(':', '')
